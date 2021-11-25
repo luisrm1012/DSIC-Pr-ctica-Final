f290b2387a2893ce4663559f3f28eeb3aa49219f
LUIS ALBERTO RASQUIN MARRA <100383346@alumnos.uc3m.es>
2021-11-25 22:43:46 +0100

    login hecho

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
-const URL = "http://127.0.0.1:4000";
-const form = document.querySelector("form");
-form.addEventListener("submit", handleSubmit);
+const URL = "http://127.0.0.1:4000/";
 
-async function handleSubmit(event) {
+let userData = [];
+
+/*const form = document.querySelector("form");
+form.addEventListener("submit", login);*/
+
+async function login(event) {
   event.preventDefault();
 
   const data = new FormData(event.target);
 
   const myObject = Object.fromEntries(data.entries());
 
-  var datos;
-  /*
-  serverResponse = await fetch(URL);
-  datos = await serverResponse.json();*/
+  let email = myObject.email;
+  let password = myObject.password;
 
-  await fetch(URL) //API URL
+  await fetch(URL + email + "&" + password) //API URL
     .then((response) => response.json())
     .then((data) => {
-      datos = data;
+      userData = data;
     })
     .catch((error) => console.log("error", error));
 
-  console.log(datos);
+  if (userData.email == email) {
+    window.location.href = "home.html";
+  }
+}
+
+async function registerCliente(event) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+
+  const myObject = Object.fromEntries(data.entries());
+
+  console.log(data);
+
+  let postOptions = {
+    method: "POST",
+    body: { myObject },
+  };
+
+  await fetch(URL + "newCliente", {
+    method: "POST",
+    body: { myObject },
+  }) //API URL
+    .then((response) => response.json())
+    .then((data) => {
+      userData = data;
+    })
+    .catch((error) => console.log("error", error));
+}
+
+async function getUserName() {
+  var myData;
+  await fetch(URL + "loggedUser") //API URL
+    .then((response) => response.json())
+    .then((data) => {
+      myData = data;
+    })
+    .catch((error) => console.log("error", error));
+
+  document.getElementById("hi-user").innerHTML = myData.email;
 }
--- server.js
+++ server.js
@@ -6,27 +6,30 @@ const PORT = 4000;
 const app = express();
 app.listen(PORT);
 
+const URL = "http://127.0.0.1:5500/";
 const dbClientesPath = "clientes.json";
 const dbRestaurantesPath = "restaurantes.json";
+
+var loggedUser;
 
 /*
 <-------------------- Middleware -------------------->
 */
 
 //Allow react app use fetch API to consume my Node API
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", false);
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", false);
 
-    next();
+  next();
 });
 
 app.use(express.json());
@@ -37,23 +40,44 @@ app.use(express.urlencoded({ extended: true }));
 <-------------------- GET Requests -------------------->
 */
 
-app.get("/", (req, res) => {
-    let data = convertStringToJSON();
-    let result = null;
+app.get("/:email&:password", (req, res) => {
+  let data = convertStringToJSON();
+  let email = req.params.email;
+  let password = req.params.password;
+  let result = JSON.parse("{}");
 
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].name == "" && data[i].password == "") {
-            result = data[i];
-        }
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].email == email && data[i].password == password) {
+      result = data[i];
+      loggedUser = data[i];
+      break;
     }
+  }
 
-    if (result == null) {
-        //No existe ese usuario
-    } else {
-        //Iniciar sesión
-    }
-    res.send(data);
+  res.send(result);
 });
+
+app.get("/loggedUser", (req, res) => {
+
+  res.send(loggedUser);
+});
+
+/*
+<-------------------- POST Requests -------------------->
+*/
+
+app.post("/newCliente", (req, res) => {
+  console.log(req.body)
+  let data = convertStringToJSON();
+  data.push(req.body);
+  convertJsonToString(data);
+
+  res.send(req.body);
+});
+
+/*
+<-------------------- Extra Functions -------------------->
+*/
 
 function convertStringToJSON() {
     let read = fs.readFileSync(dbClientesPath, (err) => {});
