f06374b3dc68a1168d725bb1742e6785c45cb4e4
LUIS ALBERTO RASQUIN MARRA <100383346@alumnos.uc3m.es>
2021-11-26 10:41:50 +0100

    added modify and view user data

--- app.js
+++ app.js
@@ -10,50 +10,52 @@ form.addEventListener("submit", login);*/
 */
 
 async function login(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const data = new FormData(event.target);
+    const data = new FormData(event.target);
 
-  const myObject = Object.fromEntries(data.entries());
+    const myObject = Object.fromEntries(data.entries());
 
-  let email = myObject.email;
-  let password = myObject.password;
-  let type = myObject.tipo;
+    let email = myObject.email;
+    let password = myObject.password;
+    let type = myObject.tipo;
 
-  await fetch(URL + email + "&" + password + "&" + type) //API URL
-    .then((response) => response.json())
-    .then((data) => {
-      userData = data;
-    })
-    .catch((error) => console.log("error", error));
+    await fetch(URL + email + "&" + password + "&" + type) //API URL
+        .then((response) => response.json())
+        .then((data) => {
+            userData = data;
+        })
+        .catch((error) => console.log("error", error));
 
-  if (userData.email == email) {
-    window.location.href = "home.html";
-  }
+    if (userData.email == email) {
+        window.location.href = "home.html";
+    }
 }
 
 async function registerCliente(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const data = new FormData(event.target);
+    const data = new FormData(event.target);
 
-  const myObject = Object.fromEntries(data.entries());
+    const myObject = Object.fromEntries(data.entries());
 
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-  let postOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: JSON.stringify(myObject),
-  };
+    let postOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(myObject),
+    };
 
-  await fetch(URL + "newCliente", postOptions) //API URL
-    .then((response) => response.json())
-    .then((data) => {
-      userData = data;
-    })
-    .catch((error) => console.log("error", error));
+    await fetch(URL + "newCliente", postOptions) //API URL
+        .then((response) => response.json())
+        .then((data) => {
+            userData = data;
+        })
+        .catch((error) => console.log("error", error));
+
+    window.location.href = "login.html";
 }
 
 /*
@@ -61,27 +63,29 @@ async function registerCliente(event) {
 */
 
 async function registerRestaurante(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const data = new FormData(event.target);
+    const data = new FormData(event.target);
 
-  const myObject = Object.fromEntries(data.entries());
+    const myObject = Object.fromEntries(data.entries());
 
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-  let postOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: JSON.stringify(myObject),
-  };
+    let postOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(myObject),
+    };
 
-  await fetch(URL + "newRestaurante", postOptions) //API URL
-    .then((response) => response.json())
-    .then((data) => {
-      userData = data;
-    })
-    .catch((error) => console.log("error", error));
+    await fetch(URL + "newRestaurante", postOptions) //API URL
+        .then((response) => response.json())
+        .then((data) => {
+            userData = data;
+        })
+        .catch((error) => console.log("error", error));
+
+    window.location.href = "login.html";
 }
 
 /*
@@ -89,13 +93,53 @@ async function registerRestaurante(event) {
 */
 
 async function getUserName() {
-  var myData;
-  await fetch(URL + "loggedUser") //API URL
-    .then((response) => response.json())
-    .then((data) => {
-      myData = data;
-    })
-    .catch((error) => console.log("error", error));
+    var myData;
+    await fetch(URL + "loggedUser") //API URL
+        .then((response) => response.json())
+        .then((data) => {
+            myData = data;
+        })
+        .catch((error) => console.log("error", error));
 
-  document.getElementById("hi-user").innerHTML = myData.email;
+    document.getElementById("hi-user").innerHTML = "Hola, " + myData.email;
+}
+
+async function getProfileOnType() {
+    var myData;
+    await fetch(URL + "loggedUser") //API URL
+        .then((response) => response.json())
+        .then((data) => {
+            myData = data;
+        })
+        .catch((error) => console.log("error", error));
+
+    if (myData.tipo == "Restaurante") {
+        window.location.href = "profileRestaurante.html";
+    } else if (myData.tipo == "Cliente") {
+        window.location.href = "profileCliente.html";
+    }
+}
+
+async function getUserData() {
+    var myData;
+    await fetch(URL + "loggedUser") //API URL
+        .then((response) => response.json())
+        .then((data) => {
+            myData = data;
+        })
+        .catch((error) => console.log("error", error));
+
+    let form = document.getElementById("data-cliente");
+    form.name.value = myData.name;
+    form.lname.value = myData.lname;
+    form.email.value = myData.email;
+    form.password.value = myData.password;
+    form.tlf.value = myData.tlf;
+    form.address.value = myData.address;
+    form.city.value = myData.city;
+    form.state.value = myData.state;
+    form.zipcode.value = myData.zipcode;
+    form.creditcard.value = myData.creditcard;
+    form.ccdate.value = myData.ccdate;
+    form.cvv.value = myData.cvv;
 }
